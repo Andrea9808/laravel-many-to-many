d3c7e48cba5270b2bb0d5dba546224dd0e8b55ea
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-02-23 15:50:32 +0100

    feat:migrations

--- database/migrations/2024_02_23_142757_create_project_technology_table.php
+++ database/migrations/2024_02_23_142757_create_project_technology_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('projects', function (Blueprint $table) {
-            $table -> foreignId('type_id') -> constrained();
+        Schema::create('project_technology', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
         });
     }
 
@@ -25,10 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('projects', function (Blueprint $table) {
-
-            $table -> dropForeign('projects_type_id_foreign');
-            $table -> dropColumn('type_id');
-        });
+        Schema::dropIfExists('project_technology');
     }
 };
