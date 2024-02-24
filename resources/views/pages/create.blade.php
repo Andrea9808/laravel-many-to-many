51068012c79efd075e8f1570114813482d3c4eda
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-02-24 13:13:43 +0100

    style: form

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -3,40 +3,46 @@
     <title>Home</title>
 @endsection
 @section('content')
-    <h1 class="text-center">New Project</h1>
-    <form action="{{route('project.store')}}" method="POST">
+    <div class="container form_">
+        <h1 class="text-center">New Project</h1>
+        <form action="{{ route('project.store') }}" method="POST">
+            @csrf
+            @method('POST')
 
-        @csrf
-        @method('POST')
+            <div class="form-group">
+                <label for="name">Name:</label>
+                <input type="text" name="name" id="name" class="form-control">
+            </div>
 
-        <div>
-            <label for="name">Name:</label>
-            <input type="text" name="name" id="name">
-        </div>
-        <div>
-            <label for="description">Description:</label>
-            <input type="text" name="description" id="description">
-        </div>
-        <div>
-            <label for="technology_id">Technology:</label>
-            <select name="technology_id" id="technology_id">
-                @foreach ($technologies as $technology)
-                    <option value="{{ $technology->id }}">{{ $technology->name }}</option>
-                @endforeach
-            </select>
-        </div>
-        <div>
-            <label for="project">Type:</label>
-            <select name="type_id" id="type_id">
-                @foreach ($types as $type)
-                    <option value="{{$type->id}}">{{$type->name}}</option>
-                @endforeach
-            </select>
-        </div>
-        <div>
-            <label for="date">Date:</label>
-            <input type="date" name="date" id="date">
-        </div>
-        <button type="submit" value="CREATE" class="btn btn-primary">CREATE</button>
-    </form>
+            <div class="form-group">
+                <label for="description">Description:</label>
+                <input type="text" name="description" id="description" class="form-control">
+            </div>
+
+            <div class="form-group">
+                <label for="technology_id">Technology:</label>
+                <select name="technology_id" id="technology_id" class="form-control">
+                    @foreach ($technologies as $technology)
+                        <option value="{{ $technology->id }}">{{ $technology->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="type_id">Type:</label>
+                <select name="type_id" id="type_id" class="form-control">
+                    @foreach ($types as $type)
+                        <option value="{{ $type->id }}">{{ $type->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="date">Date:</label>
+                <input type="date" name="date" id="date" class="form-control">
+            </div>
+
+            <button type="submit" class="btn btn-primary">CREATE</button>
+        </form>
+    </div>
 @endsection
